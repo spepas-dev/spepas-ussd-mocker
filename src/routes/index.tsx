cdfec35b7dff9a42a6141075fca0dd4b02b0e4d4
jboadispepas <jboadi@spepas.com>
2025-10-06 01:46:47 +0200

    fix

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,8 +6,8 @@ import NotFound from '@/pages/NotFound';
 import SellerUssd from '@/pages/SellerUssd';
 
 export const routes = [
-  { path: '/', element: <BuyerUssd /> },
-  { path: 'seller', element: <SellerUssd /> },
+  { path: '', element: <BuyerUssd /> },
+  { path: '/seller', element: <SellerUssd /> },
   {
     path: '/*',
     element: <NotFound />
