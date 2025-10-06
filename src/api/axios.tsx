c3579fb97bef71c65e1752adf96307223fc45356
jboadispepas <jboadi@spepas.com>
2025-10-06 02:01:56 +0200

    fix

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
-const isDev = import.meta.env.DEV;
-const proxyBase = import.meta.env.VITE_PROXY_BASE_URL; // “api”
+// const isDev = import.meta.env.DEV;
+// const proxyBase = import.meta.env.VITE_PROXY_BASE_URL; // “api”
 const liveBase = import.meta.env.VITE_API_URL; // “https://api…/gateway/v1”
 
-const baseURL = isDev
-  ? `/${proxyBase}` // → “/api”
-  : liveBase; // → remote gateway
+// const baseURL = isDev
+//   ? `/${proxyBase}` // → “/api”
+//   : liveBase; // → remote gateway
+
+const baseURL = liveBase;
 
 export const apiClient = axios.create({
   baseURL,
